00ecd3bd761dd892fdee7e25c7472eadd02d1f92
Nazmus Saadat <nazmus.saadat1@gmail.com>
2023-09-11 20:35:21 +0000

    added streamlit app, hopefully fixed secret commit?

--- map-ai/scripts/streamlit-app.py
+++ map-ai/scripts/streamlit-app.py
@@ -1,3 +1,5 @@
+import streamlit as st
+import pandas as pd
 from dotenv import load_dotenv
 import os
 import openrouteservice
@@ -8,17 +10,12 @@ coords = ((8.34234,48.23424),(8.34423,48.26424))
 
 client = openrouteservice.Client(key=OPENROUTESERVICE_API_KEY) # Specify your personal API key
 routes = client.directions(coords)
-
-def get_coords(
-        location: str,
-):
-    """
-    Get the coordinates of a location.
-    """
-    coords = client.pelias_search(text=location)
-    return coords
-
 geometry = routes['routes'][0]['geometry']
 decoded = convert.decode_polyline(geometry)
-# print(get_coords("Berlin"))
-print(routes)
+
+
+route_df = pd.DataFrame(
+    decoded['coordinates'],
+    columns=['lat', 'lon'])
+
+st.map(route_df)
