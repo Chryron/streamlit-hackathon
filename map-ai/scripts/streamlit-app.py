d9acad5ff61f52ed0e66564ac1185465dafabd03
Kai Zhu <zhuk2@mcmaster.ca>
2023-09-12 17:41:50 -0400

    removed unused lines

--- map-ai/scripts/streamlit-app.py
+++ map-ai/scripts/streamlit-app.py
@@ -1,20 +1,8 @@
 import streamlit as st
-import pandas as pd
-from dotenv import load_dotenv
+
 import os
-import openrouteservice
-from openrouteservice import convert
 import pydeck as pdk
 from navigator import Navigator
-load_dotenv()
-OPENROUTESERVICE_API_KEY = os.getenv("OPENROUTESERVICE_API_KEY")
-coords = ((8.34234, 48.23424), (8.34423, 48.26424))
-
-# Specify your personal API key
-client = openrouteservice.Client(key=OPENROUTESERVICE_API_KEY)
-routes = client.directions(coords)
-geometry = routes['routes'][0]['geometry']
-decoded = convert.decode_polyline(geometry)
 
 navigator = Navigator()
 
@@ -25,12 +13,9 @@ if "messages" not in st.session_state:
     st.session_state.messages = [
         {"role": "assistant", "content": "I am your personal navigator!"}]
 
-route_df = pd.DataFrame(
-    decoded['coordinates'],
-    columns=['lat', 'lon'])
-
-
 ############################# BODY #############################
+
+st.title("Navigator")
 
 with st.container():
 
@@ -39,25 +24,23 @@ with st.container():
             st.markdown(message["content"])
 
 
-prompt = st.chat_input("Ask me for direction")
-if prompt:
-    with st.chat_message("user"):
-        st.markdown(prompt)
+    prompt = st.chat_input("Ask me for direction")
+    if prompt:
+        with st.chat_message("user"):
+            st.markdown(prompt)
 
-    with st.chat_message("assistant"):
-        with st.empty():
-            st.info("...")
-            st.session_state.result = navigator.process_query(prompt)
-            st.markdown(st.session_state.result.instructions)
+        with st.chat_message("assistant"):
+            with st.empty():
+                st.info("...")
+                st.session_state.result = navigator.process_query(prompt)
+                st.markdown(st.session_state.result.instructions)
 
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.session_state.messages.append(
-        {"role": "assistant", "content": st.session_state.result.instructions})
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.session_state.messages.append(
+            {"role": "assistant", "content": st.session_state.result.instructions})
 
 
 if "result" in st.session_state and not st.session_state.result.route_df.empty:
-    # st.map(st.session_state.result.route_df, use_container_width=False)
-
     layer = pdk.Layer(
         type='PathLayer',
         data=st.session_state.result.route_df,
